0906e3b627dcb842b8f649fecf77bcc63265a33b
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-08-11 18:22:05 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int temp = 0;
+
+    for (i = 0; i < size - 1; i++)
+    {
+
+        for (j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+            print_array(array, size);
+        }
+    }
+}
